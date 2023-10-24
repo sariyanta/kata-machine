d741ef3fffa887048e6cfb24d01062fdea026a8d
I Made Sariyanta <desar@sariyanta.com>
2023-10-24 23:24:09 +0200

    Finnish BinarySearch

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0; 
+  let hi = haystack.length
 
+  do {
+    let mid = Math.floor(lo + (hi - lo) / 2);
+    let val = haystack[mid]
+    if (val === needle) {
+      return true
+    } else if (val < needle) {
+      lo = mid + 1
+    } else {
+      hi = mid
+    }
+  } while(lo < hi); 
+  return false;
 }
